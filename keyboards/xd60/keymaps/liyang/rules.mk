12d2a81517c3bcd579ae14238165838ccea77a30
Liyang HU <git@liyang.hu>
2021-02-05 17:59:56 +0000

    keyboards/xd60/keymaps/liyang/rules.mk: +CONSOLE_ENABLE

--- keyboards/xd60/keymaps/liyang/rules.mk
+++ keyboards/xd60/keymaps/liyang/rules.mk
@@ -5,3 +5,4 @@ $(LIYANG)/keymap.c: $(LIYANG)/liyang.json $(LIYANG)/keymap.c.in
 	cat "$@.in" >> "$@"
 
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration; full: 752B w/o console, 796B w/ console
+CONSOLE_ENABLE = yes        # Console for debug; 3568B
