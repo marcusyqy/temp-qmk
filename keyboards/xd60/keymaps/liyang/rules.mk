1d52c0a444a4beb0a344994fb5d982891f51624b
Liyang HU <git@liyang.hu>
2021-02-07 12:46:18 +0000

    keyboards/xd60/keymaps/liyang/rules.mk: +COMMAND_ENABLE

--- keyboards/xd60/keymaps/liyang/rules.mk
+++ keyboards/xd60/keymaps/liyang/rules.mk
@@ -6,3 +6,4 @@ $(LIYANG)/keymap.c: $(LIYANG)/liyang.json $(LIYANG)/keymap.c.in
 
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration; full: 752B w/o console, 796B w/ console
 CONSOLE_ENABLE = yes        # Console for debug; 3568B
+COMMAND_ENABLE = yes        # Commands for debug and configuration; 1122B w/o console, 5098B w/ console
