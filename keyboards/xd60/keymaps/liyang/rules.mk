a692f3829b6cc0e1ace82e6dee7a5d3767ea55ed
Liyang HU <git@liyang.hu>
2021-02-07 12:49:25 +0000

    keyboards/xd60/keymaps/liyang/rules.mk: Unicode disable

--- keyboards/xd60/keymaps/liyang/rules.mk
+++ keyboards/xd60/keymaps/liyang/rules.mk
@@ -7,3 +7,8 @@ $(LIYANG)/keymap.c: $(LIYANG)/liyang.json $(LIYANG)/keymap.c.in
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration; full: 752B w/o console, 796B w/ console
 CONSOLE_ENABLE = yes        # Console for debug; 3568B
 COMMAND_ENABLE = yes        # Commands for debug and configuration; 1122B w/o console, 5098B w/ console
+
+UNICODE_ENABLE = no         # Unicode; 1008B
+UNICODEMAP_ENABLE = no      # Unicode map; 1394B
+UCIS_ENABLE = no            # UCIS; UC_LNX: 1806B
+
