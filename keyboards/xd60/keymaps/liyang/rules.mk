aa53ef81ab7338a9e81978274ae8871b862383bf
Liyang HU <git@liyang.hu>
2021-02-08 00:45:27 +0000

    keyboards/xd60/keymaps/liyang/rules.mk: +MOUSEKEY_ENABLE

--- keyboards/xd60/keymaps/liyang/rules.mk
+++ keyboards/xd60/keymaps/liyang/rules.mk
@@ -7,6 +7,7 @@ $(LIYANG)/keymap.c: $(LIYANG)/liyang.json $(LIYANG)/keymap.c.in
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration; full: 752B w/o console, 796B w/ console
 CONSOLE_ENABLE = yes        # Console for debug; 3568B
 COMMAND_ENABLE = yes        # Commands for debug and configuration; 1122B w/o console, 5098B w/ console
+MOUSEKEY_ENABLE = yes       # Mouse keys; pre-refactor: w/o 3958 w/ 1612B MK_3_SPEED, post: 2532B w/o 1386B w/ MK_3_SPEED,
 
 UNICODE_ENABLE = no         # Unicode; 1008B
 UNICODEMAP_ENABLE = no      # Unicode map; 1394B
