7918b7269aca31636d773ef7a41d1cd3c2f15fd3
Liyang HU <git@liyang.hu>
2021-02-17 20:12:14 +0000

    keyboards/xd60/keymaps/liyang/rules.mk: +POINTING_DEVICE_ENABLE = no # for testing action.c

--- keyboards/xd60/keymaps/liyang/rules.mk
+++ keyboards/xd60/keymaps/liyang/rules.mk
@@ -9,6 +9,8 @@ CONSOLE_ENABLE = yes        # Console for debug; 3568B
 COMMAND_ENABLE = yes        # Commands for debug and configuration; 1122B w/o console, 5098B w/ console
 MOUSEKEY_ENABLE = yes       # Mouse keys; pre-refactor: w/o 3958 w/ 1612B MK_3_SPEED, post: 2532B w/o 1386B w/ MK_3_SPEED,
 
+POINTING_DEVICE_ENABLE = no # for testing action.c
+
 UNICODE_ENABLE = no         # Unicode; 1008B
 UNICODEMAP_ENABLE = no      # Unicode map; 1394B
 UCIS_ENABLE = no            # UCIS; UC_LNX: 1806B
